4de2c9192fcbb7de357a9998a2fda23fccf4d403
kamalpreetgrewal <kkamalpreet96@yahoo.in>
2013-03-21 08:01:41 +0530

    Update main.cpp

--- file handling/main.cpp
+++ file handling/main.cpp
@@ -15,14 +15,14 @@ int main()
    cout << "Enter your name: ";
    cin.getline(data, 100);
 
-   // write inputted data into the file.
+   // write data into the file.
    outfile << data << endl;
 
    cout << "Enter your age: ";
    cin >> data;
    cin.ignore();
 
-   // again write inputted data into the file.
+   // again write data into the file.
    outfile << data << endl;
 
    // close the opened file.
